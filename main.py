26bc4728fdcd198826dd4ceccedf562753cf93e4
ChangayaMusic <yroker@gmail.com>
2023-03-18 18:52:30 +0100

    1st table

--- main.py
+++ main.py
@@ -1,6 +1,41 @@
 import requests
 from bs4 import BeautifulSoup
+import pandas as pd
+import csv
 
-url = 'http://books.toscrape.com/'
+url = 'http://books.toscrape.com/catalogue/a-light-in-the-attic_1000/index.html'
+
 reponse = requests.get(url)
-print(reponse)
+
+if reponse.ok:
+    print(reponse.content)
+    page = reponse.content
+
+    soup = BeautifulSoup(page, 'html.parser')
+
+    product_page_url = url
+    print(url)
+
+    titre_bs = soup.find("h1")
+    print(titre_bs.text)
+
+    #table
+    data = []
+    table = soup.find('table', {"class": "table table-striped"})
+    table_body = table.find('tbody')
+    rows = table_body.find_all('tr')
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
