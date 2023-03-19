7c3b9b52ec933b69841a97af4135331c4451a95d
ChangayaMusic <yroker@gmail.com>
2023-03-19 14:10:59 +0100

    1st table

--- main.py
+++ main.py
@@ -9,21 +9,38 @@ reponse = requests.get(url)
 
 if reponse.ok:
     print(reponse.content)
-    page = reponse.content
+    page = reponse.text
 
     soup = BeautifulSoup(page, 'html.parser')
-
+    #print(soup)
     product_page_url = url
-    print(url)
+    #print(url)
 
     titre_bs = soup.find("h1")
     print(titre_bs.text)
 
-    #table
-    data = []
-    table = soup.find('table', {"class": "table table-striped"})
-    table_body = table.find('tbody')
-    rows = table_body.find_all('tr')
+    book_table = soup.find('table', class_ = 'table table-striped').text
+    #print(book_table)
+
+    rows = soup.find_all('tr')
+
+    for row in rows:
+        categorie = row.find('th').text
+        #print(categorie)
+        inforations = row.find('td').text
+        #print(inforations)
+
+    catinfo = categorie + inforations
+
+
+
+
+
+
+
+
+
+
 
 
 
